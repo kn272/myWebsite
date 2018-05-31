4a73a58624541fe77f803faf9d1953de54fe2aa3
Kishore Gajendran <kishoregajendran95@gmail.com>
2018-05-30 20:19:26 -0400

    hello world!

--- core/http/processRequest.php
+++ core/http/processRequest.php
@@ -6,7 +6,7 @@ class processRequest
   public static function createResponse()
   {
     $requested_route = processRequest::getRequestedRoute();
-    //print_r($requested_route);
+    print_r($requested_route);
     $controller_name = $requested_route->controller;
     $controller_method = $requested_route->method;
     $controller_name::$controller_method();
@@ -14,7 +14,7 @@ class processRequest
   
   public static function getRequestedRoute()
   {
-    $requested_method = request::getRequestedMethod();
+    $request_method = request::getRequestMethod();
     $page = request::getPage();
     $action = request::getAction();
     $routes = \routes::getRoutes();
